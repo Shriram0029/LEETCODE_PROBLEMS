e40bd3fdefd3411e5022b3ff80ebdc64f6202a34
Shriram0029 <162812670+Shriram0029@users.noreply.github.com>
2026-06-23 10:14:49 +0530

    Time: 211 ms (5.7%), Space: 51.4 MB (22.26%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n<2){
+            return 0;
+        }
+        return primenumbercheck(n);
+    }
+
+    public static int primenumbercheck(int n){
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for(int i=2;i*i<n;i++){
+            for(int j=2*i;j<n;j+=i){
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for(int i=2;i<n;i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
