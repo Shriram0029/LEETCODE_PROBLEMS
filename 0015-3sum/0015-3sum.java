30e3e8106acfeba50bd9091c33d55ca8c025ef0a
Shriram0029 <162812670+Shriram0029@users.noreply.github.com>
2026-07-04 17:13:59 +0530

    Time: 36 ms (30.82%), Space: 59.3 MB (32.85%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        int n = nums.length;
-        for(int i=0;i<n;i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int left = i+1;
-            int right = n-1;
-            while(left<right){
-                int sum = nums[i] + nums[left] + nums[right];
+        for(int i=0;i<nums.length-2;i++){
+            if((i > 0) && (nums[i] == nums[i-1])){
+                continue;
+            }
+            int l = i+1;
+            int r = nums.length - 1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
                 if(sum == 0){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l < r && nums[r] == nums[r-1]) r--;
 
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
+                    l++;
+                    r--;
                 }
-                else if(sum < 0)
-                {
-                    left++;
+                else if(sum > 0){
+                    r--;
                 }
                 else{
-                    right--;
+                    l++;
                 }
             }
         }
-        return result;
+        return res;
     }
 }
