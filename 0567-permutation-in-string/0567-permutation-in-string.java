13cd1eddc6812e4c940bb52b1d836b89c6145e90
Shriram Narayanan <nshriram005@gmail.com>
2026-08-02 17:21:23 +0530

    Time: 20 ms (30.17%), Space: 44.8 MB (29.39%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,43 +1,35 @@
 class Solution {
-    public boolean match(Map<Character, Integer> map, Map<Character, Integer> maps1){
-        if(map.size() != maps1.size()) return false;
-
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-            char key = entry.getKey();
-            int val = entry.getValue();
-
-            if (!maps1.containsKey(key) || !maps1.get(key).equals(val)) {
-                return false;
-            }
-        }
-        return true;
-    }
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        if(n > m) return false;
-        HashMap<Character, Integer> maps1 = new HashMap<>();
-        for(int k=0;k<n;k++){
-            maps1.put(s1.charAt(k), maps1.getOrDefault(s1.charAt(k), 0) + 1);
+        if (s1.length() > s2.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        
-        Map<Character, Integer> map = new HashMap<>();
-        int left = 0;
-        for(int right=0;right<m;right++){
-            map.put(s2.charAt(right), map.getOrDefault(s2.charAt(right),0) + 1);
-            if(right - left + 1 > n){
+
+        int left = 0, count = s1.length();
+
+        for (int right = 0; right < s2.length(); right++) {
+            char ch = s2.charAt(right);
+
+            int val = map.getOrDefault(ch, 0);
+            if (val > 0) count--;
+            map.put(ch, val - 1);
+
+            if (count == 0) return true;
+
+            if (right - left + 1 == s1.length()) {
                 char leftChar = s2.charAt(left);
-                map.put(leftChar,map.get(leftChar) - 1);
-                if(map.get(leftChar) == 0) {
-                    map.remove(leftChar);
-                }
+
+                int leftVal = map.get(leftChar);
+                if (leftVal >= 0) count++;
+                map.put(leftChar, leftVal + 1);
+
                 left++;
             }
-            if(right - left + 1 == n){
-                if(match(map, maps1))
-                    return true;
-            }
         }
+
         return false;
     }
 }
